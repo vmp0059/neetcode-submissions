1cf4704db922d937c35cd673b69f50fbca228654
Vaishnavi <vaishnavimpatil.987@gmail.com>
2026-06-21 16:19:22 +0530

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int help(int i,int j,int dp[][]){
+        
+        if(i==0 && j==0)return 1;
+        if(i<0 || j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int up=help(i-1,j,dp);
+        int left=help(i,j-1,dp);
+
+        return dp[i][j]= up+left;
+    }
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }
+                else if(i<0 || j<0){
+                    dp[i][j]=0;
+                }
+                else {
+                    int up=0;
+                    int left=0;
+                    if(i>0)up=dp[i-1][j];
+                    if(j>0)left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
